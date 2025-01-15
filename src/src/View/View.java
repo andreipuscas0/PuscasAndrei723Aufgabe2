96e80c9bbba500e6ab4f35ff90270c674f98a254
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:28:10 +0200

    added imports for View and implemented Service in View

--- src/src/View/View.java
+++ src/src/View/View.java
@@ -1,4 +1,17 @@
 package View;
 
+import Model.Spieler;
+import Model.Vereine;
+import Service.Service;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class View {
+    private final Service service;
+
+    public View(Service service) {
+        this.service = service;
+    }
+
 }
