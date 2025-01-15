3958e084997d9852853e862eb564639ba8783378
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:22:33 +0200

    added the Vereine class and the constructor of the class

--- src/src/Model/Vereine.java
+++ src/src/Model/Vereine.java
@@ -1,4 +1,17 @@
 package Model;
 
+import java.util.List;
+
 public class Vereine {
+    private int id;
+    private String name;
+    private String stadt;
+    private List<Spieler> spielers;
+
+    public Vereine(int id, String name, String stadt, List<Spieler> spielers) {
+        this.id = id;
+        this.name = name;
+        this.stadt = stadt;
+        this.spielers = spielers;
+    }
 }
