e98c4ff763ee6102ed25e253b645cf8a170bf577
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:11:18 +0200

    created class Spieler and added the constructor of the class

--- src/src/Model/Spieler.java
+++ src/src/Model/Spieler.java
@@ -4,5 +4,12 @@ public class Spieler {
     private String name;
     private int age;
     private String position;
-    public String marktwert;
+    public int marktwert;
+
+    public Spieler(String name, int age, String position, int marktwert) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.marktwert = marktwert;
+    }
 }
