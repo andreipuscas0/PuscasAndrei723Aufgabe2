b3c52c011c18e1925bd686bae2f9435d503600c7
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:31:46 +0200

    added the menu in View

--- src/src/View/View.java
+++ src/src/View/View.java
@@ -14,4 +14,15 @@ public class View {
         this.service = service;
     }
 
+    public void displayMainMenu() {
+        System.out.println("\nFußballvereinmanagement\n");
+        System.out.println("1. Manage Spielers");
+        System.out.println("2. Manage Vereines");
+        System.out.println("3. Filter Vereines by Stadt");
+        System.out.println("4. Find Spielers by Name von Benutzer");
+        System.out.println("5. Sort Spielers by Marktwert");
+        System.out.println("0. Exit");
+        System.out.print("Choose an option: ");
+    }
+
 }
