18ab830293eb10119145f854b32e1800bd093d8a
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 19:04:18 +0200

    added imports for Controller and created the constructor for the class Controller

--- src/src/Controller/Controller.java
+++ src/src/Controller/Controller.java
@@ -1,4 +1,18 @@
 package Controller;
 
+import Service.Service;
+import View.View;
+
+import java.util.Scanner;
+
 public class Controller {
+    private final Service service;
+    private final View view;
+
+    public Controller() {
+        this.service = new Service();
+        this.view = new View(service);
+    }
+
+
 }
