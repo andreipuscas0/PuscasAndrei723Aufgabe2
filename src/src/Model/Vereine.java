d1c88f616c55443743c69b89acfea75075d7a6ce
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:23:40 +0200

    added functions getName() and setName()

--- src/src/Model/Vereine.java
+++ src/src/Model/Vereine.java
@@ -23,5 +23,12 @@ public class Vereine {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
