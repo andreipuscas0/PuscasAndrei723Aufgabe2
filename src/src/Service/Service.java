dfd7db4d9578a45a1013f0baa6b62b4ad27cfe69
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 18:54:35 +0200

    added addSpieler() function in Service

--- src/src/Service/Service.java
+++ src/src/Service/Service.java
@@ -1,4 +1,20 @@
 package Service;
 
+import Model.Spieler;
+import Model.Vereine;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Service {
+    private final List<Spieler> spielers = new ArrayList<>();
+    private final List<Vereine> vereines = new ArrayList<>();
+
+    public void addSpieler(Spieler spieler) {
+        spielers.add(spieler);
+    }
+
+
+
+
 }
