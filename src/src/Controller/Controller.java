3afb655c3ac23203a0bb90ce0a10cc4562f70321
andreipuscas <andreipuscas2004@gmail.com>
2025-01-15 19:06:21 +0200

    added run() function in Controller

--- src/src/Controller/Controller.java
+++ src/src/Controller/Controller.java
@@ -14,5 +14,23 @@ public class Controller {
         this.view = new View(service);
     }
 
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
+        do {
+            view.displayMainMenu();
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> view.manageSpielers(scanner);
+//                case 2 -> view.manageVereines(scanner);
+                case 3 -> view.filterVereinesByStadt(scanner);
+                case 4 -> view.findSpielersByName(scanner);
+//                case 5 -> view.sort(scanner);
+                case 0 -> System.out.println("Exiting...");
+                default -> System.out.println("Invalid choice. Try again.");
+            }
+        } while (choice != 0);
+    }
 }
